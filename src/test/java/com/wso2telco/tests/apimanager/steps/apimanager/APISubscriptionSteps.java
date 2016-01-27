e0ca85dbf51b3e3f15f1025d720363970be40e4a
Jayani Palansooriya <jayani.palansooriya@auxenta.com>
2016-01-27 13:57:23 +0530

    added methods to the test

--- src/test/java/com/wso2telco/tests/apimanager/steps/apimanager/APISubscriptionSteps.java
+++ src/test/java/com/wso2telco/tests/apimanager/steps/apimanager/APISubscriptionSteps.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 
 import com.wso2telco.apimanager.pageobjects.apihome.subscriptions.SubscriptionsPage;
 import com.wso2telco.tests.apimanager.base.BasicTestObject;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -43,28 +44,28 @@ public class APISubscriptionSteps extends BasicTestObject {
 	@Then("^I should see consumer Key of production$")
 	public String i_should_see_consumer_Key_of_production() throws Throwable {
 		SubscriptionsPage subpage = new SubscriptionsPage(driver);
-		String conKeyProd = subpage.validateConsKeyProd();
+		String conKeyProd = subpage.getConsKeyProd();
 		return conKeyProd;
 	}
 
 	@Then("^I should see consumer Secret of production$")
 	public String i_should_see_consumer_Secret_of_production() throws Throwable {
 		SubscriptionsPage subpage = new SubscriptionsPage(driver);
-		String secKeyProd = subpage.validateSecKeyProd();
+		String secKeyProd = subpage.geteSecKeyProd();
 		return secKeyProd;
 	}
 
 	@Then("^I should see consumer Key of sandbox$")
 	public String i_should_see_consumer_Key_of_sandbox() throws Throwable {
 		SubscriptionsPage subpage = new SubscriptionsPage(driver);
-		String conKeySand = subpage.validateConsKeySand();
+		String conKeySand = subpage.getConsKeySand();
 		return conKeySand;
 	}
 
 	@Then("^consumer Secret of sandbox$")
 	public String consumer_Secret_of_sandbox() throws Throwable {
 		SubscriptionsPage subpage = new SubscriptionsPage(driver);
-		String secKeySand = subpage.validateSecKeySand();
+		String secKeySand = subpage.getSecKeySand();
 		return secKeySand;
 	}
 
@@ -77,16 +78,19 @@ public class APISubscriptionSteps extends BasicTestObject {
 
 	@Then("^I should see \"([^\"]*)\" as Token Validity of production$")
 	public void i_should_see_as_Token_Validity_of_production(String arg1) throws Throwable {
-	    
+		SubscriptionsPage subpage = new SubscriptionsPage(driver);
+		Assert.assertTrue(subpage.isTokenValidityProd(arg1));
 	}
 
 	@When("^I click regenerate under apimanager Subscriptions page sandbox$")
 	public void i_click_regenerate_under_apimanager_Subscriptions_page_sandbox() throws Throwable {
-	    
+		SubscriptionsPage subpage = new SubscriptionsPage(driver);
+		subpage.clickReGenSand();
 	}
 
 	@Then("^I should see \"([^\"]*)\" as Token Validity of sandbox$")
 	public void i_should_see_as_Token_Validity_of_sandbox(String arg1) throws Throwable {
-	    
+		SubscriptionsPage subpage = new SubscriptionsPage(driver);
+		Assert.assertTrue(subpage.isTokenValiditySand(arg1));
 	}
 }
