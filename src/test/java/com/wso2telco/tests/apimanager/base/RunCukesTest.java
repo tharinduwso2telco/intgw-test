0563914aadb6672de2e2bf52ecd8c7abd77ea9db
SulakkhanaW <SulakkhanaW@bitbucket.org>
2016-02-10 11:17:06 +0530

    minor changes

--- src/test/java/com/wso2telco/tests/apimanager/base/RunCukesTest.java
+++ src/test/java/com/wso2telco/tests/apimanager/base/RunCukesTest.java
@@ -9,12 +9,14 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(plugin = { "pretty","html:target/cucumber-html-report","json:target/cucumber-report.json" },
 		 features = {
 
-		"src/test/resources/feature/MIIFE_RTC_011.feature"
+		"src/test/resources/feature/MIIFE_RTC_102.feature"
+		//"src/test/resources/feature/publisher.feature"
 				
 },
 glue={
 		"com.wso2telco.tests.apimanager.base",
 		"com.wso2telco.tests.apimanager.steps.apimanager",
+		"com.wso2telco.tests.apimanager.steps.apipublisher",
 		"com.wso2telco.tests.apimanager.steps.idserver"
 },
 tags = //"@smoke"
