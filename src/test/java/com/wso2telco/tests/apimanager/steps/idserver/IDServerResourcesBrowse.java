9463862b79ef0740fd7e89c91986690d89a2013f
unknown <AUX-026@SulakkhanaW.SulakkhanaW>
2016-03-03 19:21:52 +0530

    rate card related class added

--- src/test/java/com/wso2telco/tests/apimanager/steps/idserver/IDServerResourcesBrowse.java
+++ src/test/java/com/wso2telco/tests/apimanager/steps/idserver/IDServerResourcesBrowse.java
@@ -1,29 +1,12 @@
 package com.wso2telco.tests.apimanager.steps.idserver;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.StringReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Source;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.stream.StreamSource;
-
 import org.junit.Assert;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import com.wso2telco.identityserver.pageobjects.carbon.CarbonResourceBrowse;
+import com.wso2telco.test.framework.tools.xml.XMLReader;
 import com.wso2telco.tests.apimanager.base.BasicTestObject;
+import com.wso2telco.tests.util.data.RateCardReader;
 import com.wso2telco.tests.util.data.RateCardXML;
 
 import cucumber.api.java.en.Then;
@@ -56,33 +39,10 @@ public class IDServerResourcesBrowse extends BasicTestObject {
 		String rateCardText = browsePage.getRateCard();
 		RateCardXML rateCard = new RateCardXML();
 		rateCard.setRateCardXMl(rateCardText);
-		Document doc = handleXML(rateCard.getRateCardXMl());
-		NodeList nodes = doc.getElementsByTagName("Operators");
-		nodes.getLength();
-	}
-	
-	public Document handleXML(String xml) throws TransformerConfigurationException, TransformerException, TransformerFactoryConfigurationError{
-/*	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	    DocumentBuilder builder = factory.newDocumentBuilder();
-	    InputSource is = new InputSource(new StringReader(xml));
-	    builder.parse(is);*/
-/*	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-	    factory.setNamespaceAware(true);
-	    DocumentBuilder builder = factory.newDocumentBuilder();
-
-	    builder.parse(new ByteArrayInputStream(xml.getBytes()));*/
-/*		String asd = "ads";
-		InputSource is= new InputSource(new StringReader(xml));
-    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    	factory.setNamespaceAware(true);
-    	DocumentBuilder builder = null;
-    	builder = factory.newDocumentBuilder();
-    	Document doc = builder.parse(is);*/
-	    Source source = new StreamSource(new StringReader(xml));
-	    DOMResult result = new DOMResult();
-	    TransformerFactory.newInstance().newTransformer().transform(source , result);
-	    return (Document) result.getNode();
+		XMLReader xmlreader = new XMLReader();
+		Document doc = xmlreader.readXMLFile(config.getValue("rateCardXML"));
+		RateCardReader rateCardReader = new RateCardReader();
+		rateCardReader.checkXML(doc);
 	}
 
 }
