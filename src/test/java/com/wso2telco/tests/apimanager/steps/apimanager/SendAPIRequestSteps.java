a202d9d472e759e33de5c1ee1494fe709707433c
unknown <AUX-026@SulakkhanaW.SulakkhanaW>
2016-04-11 12:43:24 +0530

    api request sending steps added

---
+++ src/test/java/com/wso2telco/tests/apimanager/steps/apimanager/SendAPIRequestSteps.java
@@ -0,0 +1,17 @@
+package com.wso2telco.tests.apimanager.steps.apimanager;
+
+import com.wso2telco.apimanager.pageobjects.apihome.manager.ManagerPage;
+import com.wso2telco.tests.apimanager.base.BasicTestObject;
+
+import cucumber.api.java.en.Given;
+
+public class SendAPIRequestSteps extends BasicTestObject {
+	
+	@Given("^I execute the \"([^\"]*)\" to send jmeter scripted API requests$")
+	public void i_execute_the_to_send_jmeter_scripted_API_requests(String arg1) throws Throwable {
+		ManagerPage managerpage = new ManagerPage(driver);
+		String batFilePath = config.getValue("batFilePath") + arg1;
+		managerpage.sendAPIRequest(batFilePath);
+	}
+
+}
