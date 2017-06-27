72ea316e1c2d0e0406e7756c3457a68d46df91cf
ksrajith <ksrajith@yahoo.com>
2017-06-27 16:55:56 +0530

    added feature location

--- src/test/java/com/wso2telco/tests/apimanager/base/RunCukesTest.java
+++ src/test/java/com/wso2telco/tests/apimanager/base/RunCukesTest.java
@@ -8,21 +8,20 @@ import cucumber.api.junit.Cucumber;
 
 @CucumberOptions(plugin = { "pretty","html:target/cucumber-html-report","json:target/cucumber-report.json" },
 		 features = {
-
 		"src/test/resources/feature/"
-		//"src/test/resources/feature/publisher.feature"
-				
 },
 glue={
 		"com.wso2telco.tests.apimanager.base",
+		"com.wso2telco.tests.apimanager.steps.activitiexplorer",
 		"com.wso2telco.tests.apimanager.steps.apimanager",
 		"com.wso2telco.tests.apimanager.steps.apipublisher",
 		"com.wso2telco.tests.apimanager.steps.idserver",
 		"com.wso2telco.tests.apimanager.steps.activitymonitor",
 		"com.wso2telco.tests.apimanager.steps.mailinator"
 },
-tags = {//"@smoke"
-
+tags = {
+		//"@smoke"
+        //"@BarFileUpload"
 		"@runnerAllsteps"
 }
 )
